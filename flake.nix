c16e30eb7a9934eb4b8487deb6c7d2a9789cd6ab
rogierknoester <dev@rogierknoester.nl>
2025-03-16 12:18:57 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "dodo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            rustfmt
+            clippy
+            cargo
+            rust-analyzer
+            pkg-config
+          ];
+
+          # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      }
+    );
+
+}
